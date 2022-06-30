2bd6ea7eec170271618b3c4aab42d768e0d9c544
Ubuntu <ubuntu@ip-172-31-44-103.us-east-2.compute.internal>
2022-06-30 18:07:30 +0000

    code in folder

---
+++ backend/backend/script.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install -y git
+git clone https://github.com/sofyayub/devops-code-challenge.git
+cd devops-code-challenge/backend
+npm ci
+npm start
---
+++ frontend/frontend/script.sh
@@ -0,0 +1,6 @@
+apt-get update
+apt-get install -y git
+git clone https://github.com/sofyayub/devops-code-challenge.git
+cd devops-code-challenge/frontend
+npm ci
+npm start
